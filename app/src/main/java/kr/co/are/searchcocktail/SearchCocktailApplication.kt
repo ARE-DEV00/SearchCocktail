481da23c7f8e883ee89af11f7bf97c093476588a
김상현 <are.dev00@gmail.com>
2024-08-24 00:56:05 +0900

    Refactor: 기본 프로젝트 셋팅 및 Hlit 라이브러리 추가

---
+++ app/src/main/java/kr/co/are/searchcocktail/SearchCocktailApplication.kt
@@ -0,0 +1,15 @@
+package kr.co.are.searchcocktail
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class SearchCocktailApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
