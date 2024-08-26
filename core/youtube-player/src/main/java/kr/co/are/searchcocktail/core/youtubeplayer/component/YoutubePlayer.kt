1044a7520303f8f52700be7b1697167534f4f81a
KSH <are.dev00@gmail.com>
2024-08-26 20:19:19 +0900

    feat: core:youtube-player 모듈 추가

---
+++ core/youtube-player/src/main/java/kr/co/are/searchcocktail/core/youtubeplayer/component/YoutubePlayer.kt
@@ -0,0 +1,14 @@
+package kr.co.are.searchcocktail.core.youtubeplayer.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun YoutubePlayer(
+   videoId:String
+) {
+    Box {
+        Text(text = "YoutubePlayer")
+    }
+}
