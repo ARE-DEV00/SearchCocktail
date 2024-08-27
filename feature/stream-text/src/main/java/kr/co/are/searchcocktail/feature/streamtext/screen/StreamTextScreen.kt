2ac32152f0fb3a73a68b82714ffb93a237ada1d3
김상현 <are.dev00@gmail.com>
2024-08-27 11:12:55 +0900

    feat: 유튜브 재생시, 0.1 단위로 이벤트 처리, 실시간 텍스트 하이라이트 처리관련 레이아웃 및 Text 컴포넌트 추가
    - FlowRow 컴포넌트 사용시, 퍼포먼스 이슈로 커스텀 뷰 ParagraphLayout 추가 및 적용하여 퍼포먼스 이슈 해결

--- feature/stream-text/src/main/java/kr/co/are/searchcocktail/feature/streamtext/screen/StreamTextScreen.kt
+++ feature/stream-text/src/main/java/kr/co/are/searchcocktail/feature/streamtext/screen/StreamTextScreen.kt
@@ -1,6 +1,7 @@
 package kr.co.are.searchcocktail.feature.streamtext.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,17 +10,26 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kr.co.are.searchcocktail.core.youtubeplayer.component.YoutubePlayer
 import kr.co.are.searchcocktail.domain.entity.streamtext.ParagraphChildEntity
+import kr.co.are.searchcocktail.feature.streamtext.component.KaraokeText
+import kr.co.are.searchcocktail.feature.streamtext.component.ParagraphLayout
+import kr.co.are.searchcocktail.feature.streamtext.component.TextParagraphLayout
 import kr.co.are.searchcocktail.feature.streamtext.model.StreamTextUiState
 import timber.log.Timber
 
@@ -29,47 +39,51 @@ fun StreamTextScreen(
 ) {
     val streamTextUiState by viewModel.streamTextUiState.collectAsStateWithLifecycle()
 
-    Column {
-        YoutubePlayer(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(300.dp),
-            bridgeName = "SearchCocktail",
-            videoId = "Dd1N9NrPt3A",
-            onPlayTimeUpdated = { time ->
-                Timber.d("#### onPlayTimeUpdated-${time}")
-                viewModel.updatePlayTime(time)
-            }
-        )
 
-        when (val uiState = streamTextUiState) {
-            is StreamTextUiState.Loading -> {
-                Text(text = "로딩 중")
-            }
 
-            is StreamTextUiState.Error -> {
-                Text(text = "에러 발생")
-            }
+    when (val uiState = streamTextUiState) {
+        is StreamTextUiState.Loading -> {
+            StreamTextLoading()
+        }
 
-            is StreamTextUiState.Success -> {
-                LazyColumn(
+        is StreamTextUiState.Error -> {
+            StreamTextError()
+        }
+
+        is StreamTextUiState.Success -> {
+            Column {
+                YoutubePlayer(
                     modifier = Modifier
-                        .fillMaxSize()
-                ) {
+                        .fillMaxWidth()
+                        .height(300.dp),
+                    bridgeName = "SearchCocktail",
+                    videoId = uiState.streamTextInfo?.videoUrl ?: "",
+                    onPlayTimeUpdated = { time ->
+                        Timber.d("#### onPlayTimeUpdated-${time}")
+                        viewModel.updatePlayTime(time)
+                    }
+                )
+                TextParagraphLayout {
                     uiState.streamTextInfo?.lexicalEntity?.editorState?.root?.children?.let { children ->
-                        items(children) { item ->
-                            item.children.forEach { child ->
-                                when (child) {
-                                    is ParagraphChildEntity.KaraokeEntity -> {
-                                        val formattedNumber = "%.1f".format(uiState.playTime)
-                                        val isHighlighted = child.s <= formattedNumber.toDouble() && formattedNumber.toDouble() <= child.e
-                                        KaraokeText(child, isHighlighted)
-                                    }
+                        children.forEach { child ->
+                            child.children.forEach {
+                                when (it) {
                                     is ParagraphChildEntity.SpeakerBlockEntity -> {
 
                                     }
+
+                                    is ParagraphChildEntity.KaraokeEntity -> {
+                                        KaraokeText(
+                                            it,
+                                            isHighlighted = uiState.playTime >= it.s && uiState.playTime <= it.e,
+                                            onTabText = { startTime, endTime ->
+                                                Timber.d("#### onTabText-:${startTime}-${endTime}")
+                                            }
+                                        )
+                                    }
                                 }
                             }
+
                         }
                     }
                 }
@@ -79,15 +93,16 @@ fun StreamTextScreen(
 }
 
 @Composable
-fun KaraokeText(child: ParagraphChildEntity.KaraokeEntity, isHighlighted: Boolean) {
-    Box(
-        modifier = Modifier
-            .background(if (isHighlighted) Color(0xFFADD8E6) else Color.Transparent)
-            .padding(4.dp)
-    ) {
-        Text(
-            text = child.text,
-            style = if (isHighlighted) TextStyle(background = Color(0xFFADD8E6)) else TextStyle()
-        )
+private fun StreamTextLoading() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        CircularProgressIndicator()
     }
 }
+
+@Composable
+private fun StreamTextError() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(text = "오류가 발생하였습니다.")
+    }
+}
+
