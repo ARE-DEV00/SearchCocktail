4e4d858180bac4e40523d72b682ae933743ee7b7
김상현 <are.dev00@gmail.com>
2024-08-29 14:53:57 +0900

    feat: Room DB 정렬을 위한 컬럼 추가 및 즐겨찾기 조회 쿼리 정렬 조건 추가

--- data/local-room-cocktail/src/main/java/kr/co/are/searchcocktail/data/localroomcocktail/entity/TableFavoriteDrinkInfoEntity.kt
+++ data/local-room-cocktail/src/main/java/kr/co/are/searchcocktail/data/localroomcocktail/entity/TableFavoriteDrinkInfoEntity.kt
@@ -3,6 +3,9 @@ package kr.co.are.searchcocktail.data.localroomcocktail.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kr.co.are.searchcocktail.data.localroomcocktail.CocktailDataBaseKey
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZoneOffset
 
 @Entity(tableName = CocktailDataBaseKey.TABLE_FAVORITE_DRINK_INFO)
 data class TableFavoriteDrinkInfoEntity(
@@ -13,5 +16,9 @@ data class TableFavoriteDrinkInfoEntity(
     val name: String?,                       //이름
     val thumbUrl: String? = null,           //썸네일
     val category: String? = null,           //카테고리
+
+    var createdTime: LocalDateTime = LocalDateTime.now(ZoneId.from(ZoneOffset.UTC)), //생성시간
+    var modifiedTime: LocalDateTime = LocalDateTime.now(ZoneId.from(ZoneOffset.UTC))  //수정시간
+
 )
 
